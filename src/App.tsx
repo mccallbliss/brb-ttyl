dfd1c0dff11270ae79e1dcacb52338f59784be68
McCall Bliss <missmccallkay@gmail.com>
2024-07-26 09:48:46 -0500

    remove unnecessary usestate

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import { useState } from 'react'
 import phoneLogo from './assets/brb-phone.png'
 import myspacePlayer from './assets/brb-ttyl-myspace-player.png'
 import './App.css'
 import Navigation from './Navigation';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Navigation />
@@ -17,6 +14,9 @@ function App() {
         <h1>remember when the internet was fun?</h1>
         <div>
           <img src={myspacePlayer} className="myspace-player" alt="Now Playing 'brb ttyl by davvn'" />
+        </div>
+        <div>
+
         </div>
       </div>
     </>
