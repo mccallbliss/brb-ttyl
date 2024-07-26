882c410111b8e33ff98928d65c1499fe5d91f150
McCall Bliss <missmccallkay@gmail.com>
2024-07-26 09:46:37 -0500

    add myspace-player png

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import phoneLogo from './assets/brb-phone.png'
+import myspacePlayer from './assets/brb-ttyl-myspace-player.png'
 import './App.css'
 import Navigation from './Navigation';
 
@@ -11,13 +12,11 @@ function App() {
       <Navigation />
       <div className="main">
         <a href="https://react.dev" target="_blank">
-          <img src={phoneLogo} className="logo brb ttyl" alt="brb ttyl logo" />
+          <img src={phoneLogo} className="logo" alt="brb ttyl logo" />
         </a>
         <h1>remember when the internet was fun?</h1>
-        <div className="card">
-          <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
-          </button>
+        <div>
+          <img src={myspacePlayer} className="myspace-player" alt="Now Playing 'brb ttyl by davvn'" />
         </div>
       </div>
     </>
